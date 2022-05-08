57131d10db9caf790313bbbd4545f6e724f8fd6d
Matheus Sam <92865158+MatheSam@users.noreply.github.com>
2022-05-08 18:50:17 -0300

    Ativando

--- src/script.js
+++ src/script.js
@@ -1,52 +1,55 @@
-const body = document.querySelector('body');
+function makeAll(){
+  const body = document.querySelector('body');
 
-const divTrivia = document.createElement('div');
-const divYear = document.createElement('div');
-const divDate = document.createElement('div');
+  const divTrivia = document.createElement('div');
+  const divYear = document.createElement('div');
+  const divDate = document.createElement('div');
 
-const inputTrivia = document.createElement('input');
-const inputYear = document.createElement('input');
-const inputdate = document.createElement('input');
+  const inputTrivia = document.createElement('input');
+  const inputYear = document.createElement('input');
+  const inputdate = document.createElement('input');
 
-inputTrivia.placeholder = 'Digite um número qualquer';
-inputYear.placeholder = 'Digite um ano';
-inputdate.placeholder = 'mm/dd';
+  inputTrivia.placeholder = 'Digite um número qualquer';
+  inputYear.placeholder = 'Digite um ano';
+  inputdate.placeholder = 'mm/dd';
 
-const pTrivia = document.createElement('p')
-const pYear = document.createElement('p')
-const pDate = document.createElement('p')
+  const pTrivia = document.createElement('p')
+  const pYear = document.createElement('p')
+  const pDate = document.createElement('p')
 
-const btn = document.createElement('button');
-btn.innerText = 'ATUALIZAR';
+  const btn = document.createElement('button');
+  btn.innerText = 'ATUALIZAR';
 
-divDate.append(inputdate, pDate);
-divYear.append(inputYear, pYear);
-divTrivia.append(inputTrivia, pTrivia);
+  divDate.append(inputdate, pDate);
+  divYear.append(inputYear, pYear);
+  divTrivia.append(inputTrivia, pTrivia);
 
-body.append(divDate, divYear, divTrivia, btn);
+  body.append(divDate, divYear, divTrivia, btn);
 
-async function trivia(number) {
-  const resp = await fetch(`http://numbersapi.com/${number}/math?json`)
-  const data = await resp.json()
-  pTrivia.innerText = data.text
+  async function trivia(number) {
+    const resp = await fetch(`http://numbersapi.com/${number}/math?json`)
+    const data = await resp.json()
+    pTrivia.innerText = data.text
+  }
+
+  async function year(number) {
+    const resp = await fetch(`http://numbersapi.com/${number}/year?json`)
+    const data = await resp.json();
+    pYear.innerText = data.text
+  }
+
+  async function mes(mes, dia) {
+    const resp = await fetch(`http://numbersapi.com/${mes}/${dia}/date?json`);
+    const data = await resp.json();
+    pDate.innerText = data.text
+  }
+
+  btn.addEventListener('click', (e) => {
+    e.preventDefault;
+    trivia(inputTrivia.value);
+    year(inputYear.value);
+    mes(+inputdate.value.slice(0,2), +inputdate.value.slice(3,5))
+  })
 }
-
-async function year(number) {
-  const resp = await fetch(`http://numbersapi.com/${number}/year?json`)
-  const data = await resp.json();
-  pYear.innerText = data.text
-}
-
-async function mes(mes, dia) {
-  const resp = await fetch(`http://numbersapi.com/${mes}/${dia}/date?json`);
-  const data = await resp.json();
-  pDate.innerText = data.text
-}
-
-btn.addEventListener('click', (e) => {
-  e.preventDefault;
-  trivia(inputTrivia.value);
-  year(inputYear.value);
-  mes(+inputdate.value.slice(0,2), +inputdate.value.slice(3,5))
-})
+makeAll();
 
